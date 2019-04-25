3e6629b66d5105d078f45ec0e2f36b62a54f9de8
mbedier2016 <mbedier2016@44-17.ad.campus-eni.fr>
2019-04-25 15:17:08 +0200

    CourseManager termniné v1

--- TPCourses/src/fr/eni/TPCourses/bll/CourseManager.java
+++ TPCourses/src/fr/eni/TPCourses/bll/CourseManager.java
@@ -26,32 +26,18 @@ public CourseManager instance;
 		this.listeDAO.insert(l);
 		}
 	
+	public List<Liste> selectionnerToutesLesListes(){
+		this.listeDAO = new DAOFactory().getListeDAO();
+		return this.listeDAO.selectAll();
+		}
 
-/*
-
-
-
-
-public void ajouterRepas(Repas r) {
-this.repasDAO = new DAOFactory().getRepasDAO();
-this.repasDAO.insert(r);
-}
-
-public List<Repas> selectionnerTousLesRepas(){
-this.repasDAO = new DAOFactory().getRepasDAO();
-return this.repasDAO.selectAll();
-}
-
-public Repas selectionnerById(int id) {
-this.repasDAO = new DAOFactory().getRepasDAO();
-return this.repasDAO.selectById(id);
-}
-
-public List<Repas> selectionnerTousLesRepasParJour(LocalDate date){
-this.repasDAO = new DAOFactory().getRepasDAO();
-return this.repasDAO.selectByDate(date);*/
 	
-	
+	public Liste selectionnerById(int id) {
+		this.listeDAO = new DAOFactory().getListeDAO();
+		return this.listeDAO.selectById(id);
+		}
+
+
 	
 	
 }
